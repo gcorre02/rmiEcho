a266a59f930179f37ce467c18035f5f71c077c9e
Guilherm Ribeiro <guilherme.ctr@gmail.com>
2014-04-16 12:29:26 +0100

    added the echo service interface

---
+++ src/EchoService.java
@@ -0,0 +1,17 @@
+/**
+ * Created by user on 16-04-2014.
+ */
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+/**
+ * An implementation of the echo service.
+ * this is the interface for the server class.
+ */
+public interface EchoService extends Remote {
+    /**
+     * Returns the same string passed as parameter
+     * @param s a string
+     * @return the same string passed as parameter
+     */
+    public String echo(String s) throws RemoteException;
+}

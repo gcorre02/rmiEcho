4152f57920f64122c8b5875192e0a7209f78857f
Guilherm Ribeiro <guilherme.ctr@gmail.com>
2014-04-16 12:19:14 +0100

    added missing ;

---
+++ src/EchoClient.java
@@ -0,0 +1,31 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by user on 16-04-2014.
+ */
+public class EchoClient {
+    public static void main(String[] args) {
+        Remote service = null;
+        try {
+            service = Naming.lookup("//127.0.0.1:1099/echo");
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        EchoService echoService = (EchoService) service;
+        try {
+            for(String input : args){
+                System.out.println(echoService.echo(input));
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
